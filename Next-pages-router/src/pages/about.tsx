f924ec6c9779980b22c965226b09c359d77a3cea
YI-JYUN HE <steve.work5478@gmail.com>
2025-02-19 17:17:17 +0800

    頁面完成

---
+++ Next-pages-router/src/pages/about.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1>about</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum nemo soluta dolore temporibus unde mollitia adipisci aliquid nesciunt recusandae nisi!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim et aperiam quod placeat deserunt est, earum aut. Sint, eveniet vitae?</p>
+    </div>
+  );
+}
+
+export default About;
